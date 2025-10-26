771f72e1c3f4b055d446877fc3931e6c1ecea238
Sparsa <1224636+Sparsa@users.noreply.github.com>
2025-10-26 14:28:12 +0100

    some more proofs

--- Tactics.v
+++ Tactics.v
@@ -144,6 +144,56 @@ Theorem injection_ex1 : forall (n m o : nat),
 
   
     
+Theorem discriminate_ex1 : forall (n m : nat) ,
+    false = true ->
+    n = m.
+Proof.
+  intros n m contra. discriminate contra. Qed.
 
+Theorem discriminate_ex2 : forall (n : nat),
+    S n = 0 ->
+    2 + 2 = 5.
+Proof. intros n H.
+       discriminate H. Qed.
 
+Example discriminate_ex3:
+  forall (X : Type) (x y z : X) (l j : list X),
+    x :: y :: l = [] ->
+    x = z.
+Proof. intros X x y z l j. intros H.
+       discriminate H. Qed.
 
+Theorem eqb_0_l : forall n,
+    0 =? n = true -> n = 0.
+Proof.
+  intros n.
+  destruct n as [| n'] eqn:E.
+  - intros H. reflexivity.
+  - intros H. discriminate H. Qed.
+
+Theorem f_equal : forall (A B : Type) (f : A -> B) ( x  y : A ),
+    x = y -> f x = f y.
+Proof. intros A B f x y. intros H.
+       rewrite H. reflexivity. Qed.
+
+Theorem eq_implies_succ_equal : forall (n m : nat),
+    n = m -> S n = S m.
+Proof. intros n m H. apply f_equal. apply H.
+       Qed.
+Theorem eq_implies_succ_equal' : forall ( n m : nat ),
+    n = m -> S n = S m.
+Proof. intros n m H. apply f_equal. apply H.
+       Qed.
+
+(*tactics on hypothesis*)
+
+Theorem S_inj : forall (n m : nat) (b : bool),
+    ((S n) =? (S m)) = b  ->
+    ( n =? m  ) = b.
+Proof.  intros n m b H. simpl in H. apply H. Qed.
+
+Theorem silly4 : forall (n m p q : nat),
+    (n = m -> p = q) ->
+    m = n ->
+    q = p.
+Proof. intros n m p q H.
